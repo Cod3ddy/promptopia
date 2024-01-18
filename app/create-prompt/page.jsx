89aca441f24f8489f5b8ef623ae2d5e8ce314929
Shadow <edwardchirwa07@gmail.com>
2024-01-18 02:09:55 +0200

    added add prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,7 +7,50 @@ import { useRouter } from "next/navigation";
 import Form from "@components/form";
 
 export const CreatePrompt = () => {
-	return <div> CreatePrompt</div>;
+	const [submitting, setsubmitting] = useState(false);
+	const { data: session } = useSession();
+	const router = useRouter();
+
+	const [post, setPost] = useState({
+		prompt: "",
+		tag: "",
+	});
+
+	// create prompt
+	const createPrompt = async (e) => {
+		e.preventDefault();
+		setsubmitting(true);
+
+		try {
+			// api call
+			const response = await fetch("/api/prompt/new", {
+				method: "POST",
+				body: JSON.stringify({
+					prompt: post.prompt,
+					userId: session?.user.id,
+					tag: post.tag,
+				}),
+			});
+
+			if (response.ok) {
+				router.push("/");
+			}
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setsubmitting(false);
+		}
+	};
+
+	return (
+		<Form
+			type="Create"
+			post={post}
+			setpost={setPost}
+			submitting={submitting}
+			handleSubmit={createPrompt}
+		/>
+	);
 };
 
 export default CreatePrompt;
